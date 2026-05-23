00c46ff4048be1ad84605f3f3555f23a9e3b4a76
NullTrace <129791218+noobsaibot04@users.noreply.github.com>
2026-05-23 15:15:45 +0500

    Added password generator

--- password-generator/main.py
+++ password-generator/main.py
@@ -1,1 +1,19 @@
+import random
+import string
 
+characters = string.ascii_letters + string.digits + string.punctuation
+
+length = input("Enter password length: ")
+
+if length.isdigit():
+    length = int(length)
+
+    password = ""
+
+    for i in range(length):
+        password += random.choice(characters)
+
+    print("Generated Password:", password)
+
+else:
+    print("Please enter numbers only.")
